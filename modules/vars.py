39876ab72c37f79f31b14f1e2c1bbeb48f7ec8d5
wddwwdw <hangerskype@gmail.com>
2025-02-13 02:39:52 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,7 +2,7 @@ from os import getenv
 
 api_id    = int(getenv("api_id", "21258655"))
 api_hash  = getenv("api_hash", "a26c4e545d570521926e47a77964cb1d")
-bot_token = getenv("bot_token", "7505306936:AAEyz0v9wXq1V2OZrwVne1TsrWFxTBXQNAE")
+bot_token = getenv("bot_token", "")
 pwtoken = getenv("pwtoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzk4OTUzNjEuMDM0LCJkYXRhIjp7Il9pZCI6IjY0YTEwMTJlMWNmOWI0MDAxOGI1MTQxZCIsInVzZXJuYW1lIjoiNjIwNzM4NDEwNyIsImZpcnN0TmFtZSI6IkFqIFB5dGhvbiIsImxhc3ROYW1lIjoiSkhBIiwib3JnYW5pemF0aW9uIjp7Il9pZCI6IjVlYjM5M2VlOTVmYWI3NDY4YTc5ZDE4OSIsIndlYnNpdGUiOiJwaHlzaWNzd2FsbGFoLmNvbSIsIm5hbWUiOiJQaHlzaWNzd2FsbGFoIn0sImVtYWlsIjoiYWpweXRob24yMjJAZ21haWwuY29tIiwicm9sZXMiOlsiNWIyN2JkOTY1ODQyZjk1MGE3NzhjNmVmIl0sImNvdW50cnlHcm91cCI6IklOIiwidHlwZSI6IlVTRVIifSwiaWF0IjoxNzM5MjkwNTYxfQ.7uuoWjj6k4NufX7Xpcpn5Vry-AdhivihnvYpJ62nIi0")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002226652008"))
 AUTH = "6820248376"
